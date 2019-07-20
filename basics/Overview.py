38337a730dbde9b0ecc0b66e86d3ecb9e70ee4d2
pkbuyan <zingalala1991@gmail.com>
2019-07-20 22:32:06 +0000

    Overview.Py File Uploaded

---
+++ basics/Overview.py
@@ -0,0 +1,31 @@
+"""
+comments
+single Line
+
+#
+
+' '
+
+" "
+
+Multiple Line comments
+
+""" """
+''' '''
+
+How to declare the variables
+
+combinations of A-Z , a-z , 0-9 & _
+But it shouldn't start with a number.
+
+#!/usr/bin/pyhton  Shebang #! and followed by Absolute path of the python software
+
+shebang is generally used to tell the kernel about "which interpreter should be used for executing the code written afterwards"
+ITs not compulsory. Whether the code is runnning (may be in IDE or may be in shell)it will follow the absolute path of the interpreter after the shebang.
+"""
+
+User_Name_001 ='Praveen'
+print(User_Name_001,type(User_Name_001),id(User_Name_001))
+
+
++ sign in the atom editor needs to be inspected.
